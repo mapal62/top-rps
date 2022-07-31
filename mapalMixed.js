e646942a412cb36c2c74ae765ec0146932bf4087
pali <mapal62@gmail.com>
2022-07-31 22:37:23 +0200

    Prepare for N choices -list valid choices in an array -follower beat the previous, first beat the last, otherwise player win, if not equal -messages accessed by an object key

--- mapalMixed.js
+++ mapalMixed.js
@@ -1,11 +1,24 @@
+//comment controlled mixed object vs. array base
 console.log('connect OK');
 
 let attempt = 5;
 // output strings constants
+const rock = 'Rock crushes scissors.';
+const paper = 'Paper covers rock.';
+const scissors = 'Scissors cuts paper.';
 const loss = 'You lose!';
 const win = 'You win!';
 const draw = 'Draw!';
 
+//----------object based----------
+/* const variants = {
+    0: 'rock',
+    1: 'paper',
+    2: 'scissors'
+}
+ */
+//----------array based-----------
+// a choice has been beaten by the next element, last by the first
 const variants = ['rock', 'paper', 'scissors'];
 const reason = {
     'rock': 'Rock crushes scissors.',
@@ -22,7 +35,13 @@ console.log(game(attempt));
 
 // ---functional part---
 function getComputerChoice() {
-    return variants[Math.floor(Math.random() * variants.length)];
+
+    //----------object based----------
+    // const choice = variants[Math.floor(Math.random() * 3)];
+    //----------array based-----------    
+    const choice = variants[Math.floor(Math.random() * variants.length)];
+
+    return choice;
 }
 
 function getUserChoice(round) {
@@ -33,9 +52,16 @@ function getUserChoice(round) {
     });
     message += `end !\nROUND: ${round + 1}`;
     while (true) {
+
+        //----------object based----------        
+        // choice = prompt(`Please ENTER rock / paper / scissors / end !\nROUND: ${round + 1}`).toLowerCase().trim();
+        //----------array based-----------
         choice = prompt(message).toLowerCase().trim();
         if (!choice || choice === 'end') {
             return 'end';
+            //----------object based----------        
+            // } else if (Object.values(variants).includes(choice)) {
+            //----------array based-----------
         } else if (variants.includes(choice)) {
             return choice;
         } else {
@@ -45,6 +71,40 @@ function getUserChoice(round) {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `${draw}`;
+    }
+
+    if (playerSelection === variants[0]) {
+        if (computerSelection === variants[1]) {
+            computer++;
+            return `${loss} ${paper}`;
+        } else {
+            player++;
+            return `${win} ${rock}`;
+        }
+    }
+
+    if (playerSelection === variants[1]) {
+        if (computerSelection === variants[2]) {
+            computer++;
+            return `${loss} ${scissors}`;
+        } else {
+            player++;
+            return `${win} ${paper}`;
+        }
+    }
+    if (playerSelection === variants[2]) {
+        if (computerSelection === variants[0]) {
+            computer++;
+            return `${loss} ${rock}`;
+        } else {
+            player++;
+            return `${win} ${scissors}`;
+        }
+    }
+}
 
 function playWithArray(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -73,6 +133,9 @@ function game(rounds) {
         }
         const computerSelection = getComputerChoice();
         console.log(`ROUND ${i + 1}: `, playerSelection, computerSelection);
+        //----------object based----------        
+        // console.log(playRound(playerSelection, computerSelection));
+        //----------array based-----------
         console.log(playWithArray(playerSelection, computerSelection));
     }
     if (player === computer) {
