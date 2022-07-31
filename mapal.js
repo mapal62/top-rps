0e6de34a2d6bb742146a982ec39ceeea7905d53a
pali <mapal62@gmail.com>
2022-07-31 16:24:46 +0200

    Make an X round loop -call a round X times -record individual results -output final score

--- mapal.js
+++ mapal.js
@@ -11,6 +11,8 @@ const variants = {
     1: 'paper',
     2: 'scissors'
 }
+let player;
+let computer;
 
 
 function getComputerChoice() {
@@ -25,29 +27,48 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === variants[0]) {
         if (computerSelection === variants[1]) {
+            computer++;
             return `${loss} ${paper}`;
         } else {
+            player++;
             return `${win} ${rock}`;
         }
     }
 
     if (playerSelection === variants[1]) {
         if (computerSelection === variants[2]) {
+            computer++;
             return `${loss} ${scissors}`;
         } else {
+            player++;
             return `${win} ${paper}`;
         }
     }
     if (playerSelection === variants[2]) {
         if (computerSelection === variants[0]) {
+            computer++;
             return `${loss} ${rock}`;
         } else {
+            player++;
             return `${win} ${scissors}`;
         }
     }
 }
 
-const playerSelection = 'scissors';
-const computerSelection = getComputerChoice();
-console.log('ROUND: ', playerSelection, computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function game(rounds) {
+    player = 0;
+    computer = 0;
+    for (let i = 0; i < rounds; i++) {
+        const playerSelection = 'rock';
+        const computerSelection = getComputerChoice();
+        console.log(`ROUND ${i+1}: `, playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if (player === computer) {
+        return `${draw} ${player} to ${computer}`;
+    } else if (player > computer) {
+        return `${win} ${player} to ${computer}`;
+    } else {
+        return `${loss} ${player} to ${computer}`;
+    }
+}
