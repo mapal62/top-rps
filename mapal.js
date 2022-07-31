227dec814a0b013b20e568a3e5acdf1c9d8710ea
pali <mapal62@gmail.com>
2022-07-31 15:16:18 +0200

    Create random choice -define output strings -create getComputerChoice function

--- mapal.js
+++ mapal.js
@@ -1,1 +1,20 @@
 console.log('connect OK');
+// output strings constants
+const rock = 'rock crushes scissors';
+const paper = 'paper covers rock';
+const scissors = 'scissors cuts paper';
+const loss = 'You lose!';
+const win = 'You win!';
+const draw = 'Draw!';
+
+
+function getComputerChoice() {
+    const variants = {
+        0: 'rock',
+        1: 'paper',
+        2: 'scissors'
+    }
+    const choice = variants[Math.floor(Math.random()*3)];
+    return choice;
+}
+
